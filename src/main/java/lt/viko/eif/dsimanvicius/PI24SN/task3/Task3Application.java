22d4fa2dbe9e2d3c5fb947bf17b4875131affcc7
D0mazo <dominykas.simanavicius@proton.me>
2026-05-11 12:39:58 +0300

    First Commit

---
+++ src/main/java/lt/viko/eif/dsimanvicius/PI24SN/task3/Task3Application.java
@@ -0,0 +1,13 @@
+package lt.viko.eif.dsimanvicius.PI24SN.task3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task3Application.class, args);
+	}
+
+}
